15d07b572afdb2dc86afa7bbf1aca8cf967f2a06
Emmanuel Tweneboah <twentekghana@gmail.com>
2019-10-14 11:49:55 +0800

    Project setup

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.send('HICOTEK GHANA')
+})
+
+const PORT = process.env.PORT || 9090;
+app.listen(PORT, ()=> {
+    console.log(`The server is runing on port ${PORT}`)
+})
---
+++ routes/users-route.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+const usersRouter = express.Router();
+
+
+
+
+module.exports = usersRouter;
